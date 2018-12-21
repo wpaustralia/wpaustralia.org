4572ac7f4602c578b3cc40e8ea0524ae0cff9c67
Dion Hulse <contact@dd32.id.au>
2018-12-21 19:56:47 +1100

    Remove WP_AUTO_UPDATE_CORE from the default wp-config.php file, Plesk handles that in production, but not development.

--- wp-config.php
+++ wp-config.php
@@ -2,9 +2,6 @@
 
 // WARNING: This file is public.
 
-// This setting was defined by the Plesk WordPress Toolkit to prevent WordPress auto-updates. Do not change it to avoid conflicts with the WordPress Toolkit auto-updates feature.
-define('WP_AUTO_UPDATE_CORE', false);
-
 // Include the Database Credentials, Salts and keys, etc
 @include( __DIR__ . '/wp-config-private.php' );
 if ( ! defined( 'DB_PASSWORD' ) ) {
