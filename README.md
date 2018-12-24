49abd599baa24fed5c4a5d1ffd05ae1499d1f098
Dion Hulse <contact@dd32.id.au>
2018-12-24 19:09:30 +1100

    Have the wp-config.php file be private by default, and track public constants in a wp-config-public.php file instead. This is needed as Plesk modifys the wp-config.php file and the git link will override it.

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ This in the process of being converted to a WordPress site.
 ## Setup
 
 1. WordPress files should be installed in `.`, with the git as the root directory.
+1. Install WordPress with `wp-config.php` including `wp-config-public.php` before the final `include .. wp-settings..` line.
 1. Ensure any plugins are installed.
 1. Enjoy.
---
+++ wp-config-public.php
@@ -0,0 +1,14 @@
+<?php
+
+// WARNING: This file is public.
+
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', 'wpaustralia.org' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+define( 'SUNRISE', true );
+
+// define( 'NOBLOGREDIRECT', 'https://wpaustralia.org/' );
+
